20ce6c0e0f71ff9e66ab85e955d4beec905e60b2
LeeYonggi <rl4459@naver.com>
2019-02-06 16:00:08 +0100

    Add Character, PlayerController

---
+++ Assets/Scrips/Player/CharacterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float speed = 7.0f;
+    private Rigidbody mRb;
+
+	// Use this for initialization
+	void Start () {
+        mRb = GetComponent<Rigidbody>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Vector3 CharacterMove(Vector3 mVector3)
+    {
+        transform.Translate(mVector3 * speed * Time.deltaTime);
+        return mVector3;
+    }
+
+    public float CharacterJump(float speed)
+    {
+        mRb.AddForce(new Vector3(0, speed));
+        return speed;
+    }
+}
---
+++ Assets/Scrips/Player/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float jumpSpeed;
+
+    private CharacterController mController;
+    private Rigidbody mRb;
+
+	// Use this for initialization
+	void Start () {
+        mController = GetComponent<CharacterController>();
+        mRb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        if (move.x != 0)
+            PlayerMove(move);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerJump(jumpSpeed);
+        }
+    }
+
+    void PlayerMove(Vector3 move)
+    {
+        mController.CharacterMove(move);
+        if(move.x < -0.01)
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        else
+            transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+    }
+
+    void PlayerJump(float _speed)
+    {
+        mController.CharacterJump(_speed);
+    }
+}
