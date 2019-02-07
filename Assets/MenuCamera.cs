6ee0d8afedf11cf32cd342157ff726d908148bdc
LeeYonggi <rl4459@naver.com>
2019-02-07 17:46:40 +0100

    Add MainMenu

---
+++ Assets/MenuCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamera : MonoBehaviour {
+
+    [SerializeField] private List<Vector3> point;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = Lerp(transform.position, point[0], 0.1f);
+	}
+
+    Vector3 Lerp(Vector3 p1, Vector3 p2, float d)
+    {
+        return p1 + (p2 - p1) * d;
+    }
+}
---
+++ Assets/SceneManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManager : MonoBehaviour {
+
+    public static SceneManager instance = null;
+	// Use this for initialization
+	void Start () {
+        instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeScene(string SceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+    }
+}
