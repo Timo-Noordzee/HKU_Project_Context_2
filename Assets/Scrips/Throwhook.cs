4558e80ecbe867be656255d6e8f3031af2f000e9
LeeYonggi <rl4459@naver.com>
2019-02-08 11:10:27 +0100

    a

---
+++ Assets/Scrips/Throwhook.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throwhook : MonoBehaviour {
+
+    public GameObject hook;
+
+    GameObject curHook;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
